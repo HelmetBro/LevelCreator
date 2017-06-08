a202ad9835e2c998ceaf4c9f39a4592795daf356
Eric Parsons <ericparsons@live.com>
2017-06-08 14:36:39 -0700

    Reduced CPU load.

--- src/com/LevelEditor/UpdatePaint.java
+++ src/com/LevelEditor/UpdatePaint.java
@@ -6,11 +6,13 @@ public class UpdatePaint {
         remakeRightY();
         remakeTopX();
         remakeWindow();
+        delay();
     }
 
     public static void remakeWindow() {
         ApplicationWindow.canvas.repaint();
         ApplicationWindow.scrollHolder.repaint();
+        delay();
     }
 
     private static void remakeRightY() {
@@ -19,6 +21,13 @@ public class UpdatePaint {
 
     private static void remakeTopX() {
         ApplicationWindow.backTopXPanel.repaint();
+    }
+
+    //artificial method to reduce delay
+    private static void delay(){
+        try {
+            Thread.sleep(2);
+        } catch (InterruptedException ignore) {}
     }
 
 }
